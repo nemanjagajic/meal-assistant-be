7ad8fa26758cf0a2fb8ac7b15d1fa323bbc4dba9
Nemanja Gajic <nemanja@MacBook-Pro-2.local>
2022-03-07 14:12:17 +0100

    configured database.js for production

--- config/database.js
+++ config/database.js
@@ -1,11 +1,22 @@
 const path = require('path');
 
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      options: {},
     },
-    useNullAsDefault: true,
   },
 });
